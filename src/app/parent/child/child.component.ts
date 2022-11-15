87cd0db0a02ce4e28425cbfdcf72b84d59d56cfa
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-11-15 18:00:56 +0300

    new

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -1,18 +1,14 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-export interface Grade {
-  math: number;
-  physic: number;
-}
+
 @Component({
   selector: 'inst-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
 export class ChildComponent {
-  @Output() sendGrateEvent = new EventEmitter<Grade>();
+  @Output() sendGrateEvent = new EventEmitter<string>();
+  inputGrade = '';
   sendGradeHandler() {
-    const math = 5;
-    const physic = 4;
-    this.sendGrateEvent.emit({ math, physic });
+    this.sendGrateEvent.emit(this.inputGrade);
   }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,17 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { Grade } from './child/child.component';
-
+export interface WeekGrade {
+  id: number;
+  gradeItem: number;
+}
+export interface Lesson {
+  id: number;
+  title: string;
+  weekGrades: WeekGrade[];
+}
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  math?: number;
-  physic?: number;
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 2,
+        },
+        {
+          id: 1,
+          gradeItem: 4,
+        },
+        {
+          id: 2,
+          gradeItem: 3,
+        },
+      ],
+    },
+    {
+      id: 0,
+      title: 'Physic',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 4,
+        },
+        {
+          id: 1,
+          gradeItem: 2,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+  ];
+  isLoading = true;
 
-  getGrate(value: Grade) {
-    this.math = value.math;
-    this.physic = value.physic;
+  constructor() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
+  getGrate(grade: string) {
+    // this.grades.push(grade);
   }
 }
