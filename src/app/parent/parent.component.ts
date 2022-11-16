5eec2ce705d4e9167adcc1db9f43495509420bd4
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-11-16 11:09:40 +0300

    ngSwitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -14,52 +14,5 @@ export interface Lesson {
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  lessons: Lesson[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 2,
-        },
-        {
-          id: 1,
-          gradeItem: 4,
-        },
-        {
-          id: 2,
-          gradeItem: 3,
-        },
-      ],
-    },
-    {
-      id: 0,
-      title: 'Physic',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 4,
-        },
-        {
-          id: 1,
-          gradeItem: 2,
-        },
-        {
-          id: 2,
-          gradeItem: 5,
-        },
-      ],
-    },
-  ];
-  isLoading = true;
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 3000);
-  }
-  getGrate(grade: string) {
-    // this.grades.push(grade);
-  }
+  value = '';
 }
